f36af5130c2d5053c6476ceb07ff028974ace26c
mderkach <derkach94@gmail.com>
2020-07-23 11:58:23 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/extensions */
+// JS
+// eslint-disable-next-line import/no-unresolved
+import './js/*';
+// SCSS
+import './scss/main.scss';
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// components
