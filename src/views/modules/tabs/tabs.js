9c1d7eb4737cdff6c28475bef19f802823893af7
Skeev_ <derkach94@gmail.com>
2020-08-03 12:24:18 +0300

    tabs nav slider

--- src/views/modules/tabs/tabs.js
+++ src/views/modules/tabs/tabs.js
@@ -1,6 +1,38 @@
+import { Swiper, Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+
 const tabs = {
   navBtns: document.querySelectorAll('.tabs-menu__button'),
   navAnchors: document.querySelectorAll('.tabs-menu__link'),
+  navSlider: new Swiper('.tabs-menu__link_wrapper', {
+    init: false,
+    slidesPerView: 3,
+    wrapperClass: 'tabs-menu__link_outer',
+    slideClass: 'tabs-menu__link',
+    speed: 400,
+    navigation: {
+      nextEl: document
+        .querySelector('.tabs-menu__link_wrapper')
+        .parentElement.querySelector('.tabs-menu__button_next'),
+      prevEl: document
+        .querySelector('.tabs-menu__link_wrapper')
+        .parentElement.querySelector('.tabs-menu__button_prev'),
+      disabledClass: 'is-disabled',
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+    },
+  }),
   currentAnchor: undefined,
   tabs: document.querySelectorAll('.tabs-tab'),
   getCurrentTab: (arr) => {
@@ -26,6 +58,29 @@ const tabs = {
       item.classList.remove('is-active');
     });
   },
+  // prevAll: (elm) => {
+  //   const result = [];
+  //   let from = elm;
+  //   while (from) {
+  //     result.push(from);
+  //     from = from.previousElementSibling;
+  //   }
+
+  //   let offset = 0;
+
+  //   result.forEach((link, index) => {
+  //     if (index !== 0) {
+  //       const { width } = link.getBoundingClientRect();
+  //       console.log(link.getBoundingClientRect());
+  //       offset += Math.floor(width);
+  //     }
+  //   });
+
+  //   tabs.navAnchors.forEach((item) => {
+  //     // eslint-disable-next-line no-param-reassign
+  //     item.style.transform = `translateX(${-offset - 15}px)`;
+  //   });
+  // },
   changeTab: () => {
     tabs.navBtns.forEach((btn) => {
       btn.addEventListener('click', (e) => {
@@ -36,26 +91,31 @@ const tabs = {
         tabs.getCurrentTab(tabs.navAnchors);
         // remove active class on anchor
         tabs.removeActiveClass(tabs.navAnchors);
+        // next & prev elements
+        const next = tabs.currentAnchor.nextElementSibling || tabs.currentAnchor.nextSibling;
+        const prev = tabs.currentAnchor.previousElementSibling || tabs.currentAnchor.prevSibling;
+        const first = tabs.navAnchors[0];
+        const last = tabs.navAnchors[tabs.navAnchors.length - 1];
 
         // set active next anchor
         if (btn.classList.contains('tabs-menu__button_next')) {
-          if (tabs.currentAnchor.nextElementSibling) {
-            tabs.currentAnchor.nextElementSibling.classList.add('is-active');
-            tabs.setCurrentTab(tabs.tabs, tabs.currentAnchor.nextElementSibling);
+          if (next) {
+            next.classList.add('is-active');
+            tabs.setCurrentTab(tabs.tabs, next);
           } else {
-            tabs.navAnchors[0].classList.add('is-active');
-            tabs.setCurrentTab(tabs.tabs, tabs.navAnchors[0]);
+            first.classList.add('is-active');
+            tabs.setCurrentTab(tabs.tabs, first);
           }
         }
 
         // set active prev anchor
         if (btn.classList.contains('tabs-menu__button_prev')) {
-          if (tabs.currentAnchor.previousElementSibling) {
-            tabs.currentAnchor.previousElementSibling.classList.add('is-active');
-            tabs.setCurrentTab(tabs.tabs, tabs.currentAnchor.previousElementSibling);
+          if (prev) {
+            prev.classList.add('is-active');
+            tabs.setCurrentTab(tabs.tabs, prev);
           } else {
-            tabs.navAnchors[tabs.navAnchors.length - 1].classList.add('is-active');
-            tabs.setCurrentTab(tabs.tabs, tabs.navAnchors[tabs.navAnchors.length - 1]);
+            last.classList.add('is-active');
+            tabs.setCurrentTab(tabs.tabs, last);
           }
         }
       });
@@ -78,6 +138,7 @@ const tabs = {
     tabs.setCurrentTab(tabs.tabs, tabs.currentAnchor);
     tabs.changeTab();
     tabs.anchorClick();
+    tabs.navSlider.init();
   },
 };
 
